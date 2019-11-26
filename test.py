090c0942005297b4ab8ac407bd6fe90b479d06b8
Mkrtich Urumyan <mkrtich.urumyan@gmail.com>
2019-11-26 20:45:57 +0000

    two portfolios with different weights compared

--- test.py
+++ test.py
@@ -22,9 +22,13 @@ mydata.iloc[0]
 returns = (mydata/mydata.shift(1))-1
 returns.head()
 weights = np.array([0.25, 0.25, 0.25, 0.25])
-np.dot(returns,weights)
 annual_returns = returns.mean()*250
+np.dot(annual_returns,weights)
 print(annual_returns)
 np.dot(annual_returns,weights)
 portfolio_1=str(round(np.dot(annual_returns,weights),5)*100)+'%'
+weights2 = np.array([0.4, 0.4, 0.015, 0.05])
+np.dot(annual_returns,weights2)
+portfolio_2=str(round(np.dot(annual_returns,weights2),5)*100)+'%'
 print(portfolio_1)
+print(portfolio_2)
