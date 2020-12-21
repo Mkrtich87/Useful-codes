d8df2e008be5cd978755283861a3b54e6857172f
Mkrtich87 <52213410+Mkrtich87@users.noreply.github.com>
2020-12-21 01:49:19 -0500

    Update Absenteism Project.py

--- Absenteism Project.py
+++ Absenteism Project.py
@@ -8,11 +8,13 @@ pd.options.display.max_rows = 12
 df = df.drop(["ID"], axis=1)
 df['Reason for Absence'].max()
 df['Reason for Absence'].unique()
-reason_columns =pd.get_dummies(['Reasons for Absence'])
+reason_columns =pd.get_dummies(df['Reason for Absence'])
+print(reason_columns)
 print(reason_columns)
 reason_columns['check'] = reason_columns.sum(axis=1)
 reason_columns['check'].sum(axis=0)
 reason_columns['check'].unique
 reason_columns = reason_columns.drop(['check'],axis=1)
-reason_columns =pd.get_dummies(['Reasons for Absence'], drop_first =True)
+reason_columns =pd.get_dummies(df['Reason for Absence'], drop_first =True)
+
 print(reason_columns)
